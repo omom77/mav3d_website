2513ee3a2c6d801c3a31bf20aca36ec2cf00b75e
Om Sakharkar <sakharkarom777@gmail.com>
2024-09-13 19:55:08 +0530

    tailwind for navbar and img done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,38 @@ module.exports = {
     "./js/main.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      // WARNING!! Below is INCORRECT!
+      // backgroundImage:{
+      //   'main_bg':"url('../img/3_background_img.jpg)"
+      // }
+      minHeight: {
+        '128': '32rem',
+      }
+  },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1200px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'darkcyan': '#BFD1D5',
+      'lightcyan': '#DFE7EA',
+      'white': '#ffffff'
+    },
+    fontFamily: {
+        sans: ['Outfit', 'sans-serif']
+    },
+    // borderRadius: {
+    //     '4xl': '2rem',
+    //   }
   },
   plugins: [],
 }
